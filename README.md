04be753b267b74d23570489c82622861c65d4469
Rob Merrill <robgmerrill@gmail.com>
2016-05-02 10:25:24 -0700

    installed ui router

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # mean-stack-olympics
+
+Mean Stack App: Summer Olympics
+Features: MongoDB, Express, Angular and Node
+Extra Features: Gulp, deployed on Heroku
--- client/src/app.js
+++ client/src/app.js
@@ -1,13 +1,15 @@
-// import angular from 'angular'
-// angular.module('olympics', [])
-// .controller('sportsController', function($http) {
-//   $http.get('/sports').then((response) => {
-//     this.sports = response.data;
-//   });
-// })
-
 import angular from 'angular'
-angular.module('olympics', [])
+import 'angular-ui-router'
+angular.module('olympics', ['ui.router'])
+.config(($stateProvider, $urlRouterProvider) => {
+  $urlRouterProvider.otherwise('/sports')
+
+  $stateProvider
+  .state('sports', {
+    url: '/sports',
+    templateUrl: 'sports/sports-nav.html'
+  })
+})
 .controller('sportsController', function($http) {
   $http.get('/sports').then((response) => {
     this.sports = response.data;
